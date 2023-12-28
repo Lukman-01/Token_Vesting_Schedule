96c0ab4116b788e0159db0f9f2aa3aceab013a85
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-12-28 10:04:25 +0100

    config

--- Backend/hardhat.config.js
+++ Backend/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
