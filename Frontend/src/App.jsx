f23e240b248f83bca442d16ec8fcb1ac8a7bdecc
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-12-28 10:15:59 +0100

    React setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>Welcome To Token-Vesting-Schedule dApp</p>
+      </div>   
+    </>
+  )
+}
+
+export default App
