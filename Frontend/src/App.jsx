c993f49b2b7b314c3e275394d3cdf2cb53cd40f2
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-12-29 00:33:40 +0100

    appjs

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import Links from './components/Links';
+import 'tailwindcss/tailwind.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
-    <>
-      <div>
-        <p>Welcome To Token-Vesting-Schedule dApp</p>
-      </div>   
-    </>
-  )
-}
+    <div>
+      <Links/>
+    </div>
+  );
+};
 
-export default App
+export default App;
+
+// CustomToken deployed to: 0x106568A18A91Ad0FFb567E761caEDDE0E47d2382
+// TokenVesting deployed to: 0x7c8EF04F30Ea74B14Ded5A210bB8288382BE140b
+// TokenVestingVII deployed to: 0xDfb03c6dDC0cfca949a65A5d14Ce6c564a5fB918
